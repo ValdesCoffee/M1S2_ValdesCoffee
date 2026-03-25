e5b25744686aeb97a04d48ad3541c2799e18805b
ValdesCoffee <valdes.2022806@ensdbexcelencia.edu.co>
2026-03-25 13:56:48 -0500

    creando menu

---
+++ main.py
@@ -0,0 +1,13 @@
+productos = []
+
+def mostraremuñoa():
+    print("\n1) Agregar producto\n2)Mostrar inventario\n3)Calcular estadisticas")
+    print("\n 4) Salir (❁´◡`❁)")
+
+
+while True:
+    mostraremuñoa()
+    sdk= input("Elige una opcion por favor: ")
+
+    if sdk== "1":   
+        break
