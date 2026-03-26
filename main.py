80450e65df6e19737bbe2c803f3b9efd1d49c6ac
Valdes.Coffee <valdes.2022806@ensdbexcelencia.edu.co>
2026-03-26 12:17:29 -0500

    Task 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-inventario = []
+productos = []
 def mostraremuñoa():
     print("\n1) Agregar producto\n2)Mostrar inventario\n3)Calcular estadisticas")
     print("\n 4) Salir (❁´◡`❁)")
@@ -13,20 +13,38 @@ def agregar_producto():
         "cantidad": cantidad
     }
 
-    inventario.append(producto)
+    productos.append(producto)
     print("✓ Producto agregado")
-
+def Mostrarproducto(productos):
+    for idx, mostrar in enumerate(productos):
+        print(f"{idx+1}. Producto: {mostrar['Producto']} | Precio: {mostrar['Precio']} | Cantidad: {mostrar['Cantidad']} | Categoria: {mostrar['Categoria']}")
+    print("Listo ☆")
+def Agregarproducto(nombre,precio, cantidad, categoria, lista):
+    nuevo_producto ={
+        "Producto": nombre, 
+        "Precio": precio,
+        "Cantidad": cantidad,
+        "Categoria": categoria,
+    }
+    lista.append(nuevo_producto)
 while True:
     mostraremuñoa()
-    opcion = input("Elige una opción: ")
+    sdk = input("Elige una opción: ")
 
-    if opcion == "1":
-        agregar_producto()
-    elif opcion == "2":
+    if sdk== "1":   
+        cantidad_a_registrar= int(input("Ingrese la cantidad de productos que va a registrar: "))
+        for producto in range(cantidad_a_registrar):
+            iti= input("Coloque su producto: ")
+            precio= float(input("Coloque el precio: "))
+            cantidadeofjoijgi= int(input("Coloque la cantidad de su producto: "))
+            categoria= input("❁ Coloque su categoria:  ")
+            Agregarproducto(iti,precio,cantidadeofjoijgi,categoria,productos)
+
+    elif sdk == "2":
+         Mostrarproducto(productos)
+    elif sdk == "3":
         print("Pendiente")
-    elif opcion == "3":
-        print("Pendiente")
-    elif opcion == "4":
+    elif sdk == "4":
         break
     else:
         print(" Opción inválida")
